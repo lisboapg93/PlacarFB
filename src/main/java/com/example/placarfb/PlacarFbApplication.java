48b709f31767385e0c623ef134fd25bfd4edbfad
Pedro Lisboa <pedrogabriel3@hotmail.com>
2023-03-28 02:38:58 -0300

    atts

---
+++ src/main/java/com/example/placarfb/PlacarFbApplication.java
@@ -0,0 +1,13 @@
+package com.example.placarfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacarFbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacarFbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/placarfb/PlacarFbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placarfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacarFbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
